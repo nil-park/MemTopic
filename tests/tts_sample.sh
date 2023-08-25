cacb39ab8bbc48fd2c24931f3ccf64f85f631ae2
nil-park <kwang0621@gmail.com>
2023-08-26 00:42:41 +0900

    Update the sample GCP TTS REST API call script to move the API Key location from URL parameters to the X-goog-api-key header.

--- tests/tts_sample.sh
+++ tests/tts_sample.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-curl "https://us-central1-texttospeech.googleapis.com/v1beta1/text:synthesize?key=$GCP_TTS_API_TOKEN" \
+curl "https://us-central1-texttospeech.googleapis.com/v1beta1/text:synthesize" \
      -H "Content-Type: application/json" \
+     -H "X-goog-api-key: $GCP_TTS_API_TOKEN" \
      -d @$1
 
