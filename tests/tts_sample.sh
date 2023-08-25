74aa2b21c855b70348be550a47490dbc336dfba8
nil-park <kwang0621@gmail.com>
2023-08-26 00:42:42 +0900

    Removed old TTS API test functions

--- tests/tts_sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl "https://texttospeech.googleapis.com/v1beta1/text:synthesize" \
-     -H "Content-Type: application/json" \
-     -H "X-goog-api-key: $GCP_TTS_API_TOKEN" \
-     -d @$1
-
