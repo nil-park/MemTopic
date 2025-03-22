ab8b5119fe1eba97cb056e0af47eec2e2a53dd0d
nil-park <kwang0621@gmail.com>
2025-03-22 22:43:01 +0900

    Add `-y` option to FFmpegKit commands to overwrite output files.

--- android/app/src/main/java/com/nolbee/memtopic/utils/AudioPlayerHelper.kt
+++ android/app/src/main/java/com/nolbee/memtopic/utils/AudioPlayerHelper.kt
@@ -3,6 +3,7 @@ package com.nolbee.memtopic.utils
 import android.content.Context
 import android.media.MediaPlayer
 import android.util.Base64
+import android.util.Log
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.ReturnCode
 import java.io.File
@@ -48,7 +49,7 @@ object AudioPlayerHelper {
     private fun changeMp3Bitrate(audioFile: File, context: Context): File {
         val base64Fixed = File(context.cacheDir, "base64Fixed.mp3").apply { deleteOnExit() }
         val cmd =
-            "-i ${audioFile.absolutePath} -c:a libmp3lame -q:a 2 ${base64Fixed.absolutePath}"
+            "-y -i ${audioFile.absolutePath} -c:a libmp3lame -q:a 2 ${base64Fixed.absolutePath}"
         val session = FFmpegKit.execute(cmd)
         assert(ReturnCode.isSuccess(session.returnCode))  // TODO: Raise exception if and handle it
         return base64Fixed
@@ -57,21 +58,21 @@ object AudioPlayerHelper {
     private fun copyIntervalSoundToCacheDir(context: Context): File {
         val intervalSoundFile =
             File(context.cacheDir, "flowing_stream.mp3").apply { deleteOnExit() }
-        if (!intervalSoundFile.exists()) {
-            context.assets.open("flowing_stream.mp3").use { input ->
-                FileOutputStream(intervalSoundFile).use { output -> input.copyTo(output) }
-            }
+        if (intervalSoundFile.exists() && intervalSoundFile.length() == 2756022L) {
+            return intervalSoundFile
+        }
+        context.assets.open("flowing_stream.mp3").use { input ->
+            FileOutputStream(intervalSoundFile).use { output -> input.copyTo(output) }
         }
         return intervalSoundFile
     }
 
-    private fun cutIntervalSound(duration: Duration, context: Context): File {
-        val dm = duration.toMillis()
+    private fun cutIntervalSound(durationInMillis: Long, context: Context): File {
         val intervalSoundFile = copyIntervalSoundToCacheDir(context)
         val intervalSoundFileShort =
             File(context.cacheDir, "flowing_stream_short.mp3").apply { deleteOnExit() }
         val cmd =
-            "-i ${intervalSoundFile.absolutePath} -t ${dm}ms -c:a libmp3lame -q:a 2 ${intervalSoundFileShort.absolutePath}"
+            "-y -i ${intervalSoundFile.absolutePath} -t ${durationInMillis}ms -c:a libmp3lame -q:a 2 ${intervalSoundFileShort.absolutePath}"
         val session = FFmpegKit.execute(cmd)
         assert(ReturnCode.isSuccess(session.returnCode))  // TODO: Raise exception if and handle it
         return intervalSoundFileShort
@@ -80,18 +81,24 @@ object AudioPlayerHelper {
     private fun concatAudioFiles(f1: File, f2: File, context: Context): File {
         val mergedFile = File(context.cacheDir, "merged.mp3").apply { deleteOnExit() }
         val cmd =
-            "-i \"concat:${f1.absolutePath}|${f2.absolutePath}\" -c copy ${mergedFile.absolutePath}"
+            "-y -i \"concat:${f1.absolutePath}|${f2.absolutePath}\" -c:a libmp3lame -q:a 2 ${mergedFile.absolutePath}"
         val session = FFmpegKit.execute(cmd)
         assert(ReturnCode.isSuccess(session.returnCode))  // TODO: Raise exception if and handle it
         return mergedFile
     }
 
-    fun appendIntervalSound(audioBase64: String, context: Context): File {
+    fun appendIntervalSound(audioBase64: String, context: Context, multiplier: Float = 1.0f): File {
+        Log.d("AudioPlayerHelper", "Original Duration: ${calculateAudioDuration(audioBase64)}")
         val ttsSoundFile = saveBase64ToMp3(audioBase64, context)
         val ttsSoundFileFixedBitrate = changeMp3Bitrate(ttsSoundFile, context)
-        val duration = calculateAudioDuration(ttsSoundFileFixedBitrate)
+        val duration =
+            (calculateAudioDuration(ttsSoundFileFixedBitrate).toMillis() * multiplier).toLong()
         val intervalSoundFileShort = cutIntervalSound(duration, context)
         val mergedFile = concatAudioFiles(ttsSoundFileFixedBitrate, intervalSoundFileShort, context)
+        Log.d(
+            "AudioPlayerHelper",
+            "Interval Sound Added Duration: ${calculateAudioDuration(mergedFile)}"
+        )
         return mergedFile
     }
 }
