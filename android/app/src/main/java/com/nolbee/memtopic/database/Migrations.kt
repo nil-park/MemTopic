e85bfa9635c81f2cd9ab701a060690f66b1769ed
nil-park <kwang0621@gmail.com>
2025-04-09 00:06:25 +0900

    Add 'options' column to Topic table
    This commit introduces a new 'options' column to the Topic table for storing JSON data.
    It includes updating the database schema and adding default values to the sample topics.
    Also it adds database migration from version 4 to 5.

--- android/app/src/main/java/com/nolbee/memtopic/database/Migrations.kt
+++ android/app/src/main/java/com/nolbee/memtopic/database/Migrations.kt
@@ -14,3 +14,8 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
         )
     }
 }
+val MIGRATION_4_5 = object : Migration(4, 5) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE Topic ADD COLUMN `options` TEXT NOT NULL DEFAULT '{}'")
+    }
+}
